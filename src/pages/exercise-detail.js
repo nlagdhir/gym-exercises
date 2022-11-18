440c0adf59af755acc55aac91ed7810e43c500a1
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-11-18 21:46:13 +0530

    create exercise detail page

--- src/pages/exercise-detail.js
+++ src/pages/exercise-detail.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import Detail from "../components/detail";
+import ExerciseVideos from "../components/exercise-videos";
+import SimilarExercises from "../components/similar-exercises";
+
+import { exerciseOptions, fetchData } from "../utils/fetch-data";
 
 function ExerciseDetail() {
-  return (
-    <div>ExerciseDetail</div>
-  )
+
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async() => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUtl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      
+      setExerciseDetail(exerciseDetailData); 
+    }
+    fetchExercisesData();
+    
+  }, [id])
+  
+
+
+  return <Box>
+    <Detail exerciseDetail={exerciseDetail} />
+    <ExerciseVideos />
+    <SimilarExercises />
+  </Box>;
 }
 
-export default ExerciseDetail
+export default ExerciseDetail;
